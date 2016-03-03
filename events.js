5e57c4216dfd685abafbd89f18a9411a5436d779
Max Baldwin <maxrbaldwin@gmail.com>
2016-03-02 19:51:18 -0600

    adding DOM variables

---
+++ events.js
@@ -0,0 +1,11 @@
+// creating list of variables that will be referenced 
+var output = document.getElementByID("output-target");
+var keypress = document.getElementByID("keypress-input");
+var colorbutton = document.getElementByID("add-color");
+var guineapig = document.getElementByID("guinea-pig");
+var large = document.getElementByID("make-large");
+var border = document.getElementByID("add-border");
+var round = document.getElementByID("add-rounding");
+var header = document.getElementByID("page-header");
+var title = document.getElementByID("page-title");
+var article = document.getElementsByClassName("article-section"); //always returns array
